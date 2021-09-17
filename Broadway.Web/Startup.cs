95e481efc2b8340ef0ae7201f9397ece4bd458c5
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-09-17 09:06:23 +0545

    Web project added

---
+++ Broadway.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Broadway.Web.Startup))]
+namespace Broadway.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
