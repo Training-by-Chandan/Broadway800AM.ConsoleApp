4696ece0dae13a68c265c8c788237deb72d481ff
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-11-12 08:28:14 +0545

    Unit test project added

---
+++ EComTests/ArithmeticTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Tests
+{
+    [TestClass()]
+    public class ArithmeticTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Arithmetic a = new Arithmetic();
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+            var res = a.Add(x, y);
+            Assert.AreEqual(expected, res);
+        }
+
+        [TestMethod]
+        public void AddTestV2()
+        {
+            Arithmetic a = new Arithmetic();
+            var res = a.Add(2, 3);
+            Assert.IsInstanceOfType(res, typeof(int));
+        }
+    }
+}
